121a5b1c3ea9f04476b5b5a17c94e31e77be65e9
Edward George <edwardgeorge@gmail.com>
2011-03-08 21:21:03 +0000

    Added some documentation.

--- clonevirtualenv.py
+++ clonevirtualenv.py
@@ -1,3 +1,36 @@
+"""virtualenv cloning script.
+
+A script for cloning a non-relocatable virtualenv.
+
+Virtualenv provides a way to make virtualenv's relocatable which could then be
+copied as we wanted. However making a virtualenv relocatable this way breaks
+the no-site-packages isolation of the virtualenv as well as other aspects that
+come with relative paths and '/usr/bin/env' shebangs that may be undesirable.
+
+Also, the .pth and .egg-link rewriting doesn't seem to work as intended. This
+attempts to overcome these issues and provide a way to easily clone an
+existing virtualenv.
+
+It performs the following:
+
+- copies sys.argv[1] dir to sys.argv[2]
+- updates the hardcoded VIRTUAL_ENV variable in the activate script to the
+  new repo location.
+- updates the shebangs of the various scripts in bin to the new python if
+  they pointed to the old python. (version numbering is retained.)
+
+    it can also change '/usr/bin/env python' shebangs to be absolute too,
+    though this functionality is not exposed at present.
+
+- checks sys.path of the cloned virtualenv and if any of the paths are from
+  the old environment it finds any .pth or .egg-link files within sys.path
+  located in the new environment and makes sure any absolute paths to the
+  old environment are updated to the new environment.
+
+- finally it double checks sys.path again and will fail if there are still
+  paths from the old environment present.
+
+"""
 from __future__ import with_statement
 import logging
 import os
@@ -5,8 +38,24 @@ import shutil
 import subprocess
 import sys
 
+version_info = (0, 1)
+__version__ = '.'.join(version_info)
+
 
 def _dirmatch(path, matchwith):
+    """Check if path is within matchwith's tree.
+
+    >>> _dirmatch('/home/foo/bar', '/home/foo/bar')
+    True
+    >>> _dirmatch('/home/foo/bar/', '/home/foo/bar')
+    True
+    >>> _dirmatch('/home/foo/bar/etc', '/home/foo/bar')
+    True
+    >>> _dirmatch('/home/foo/bar2', '/home/foo/bar')
+    False
+    >>> _dirmatch('/home/foo/bar2/etc', '/home/foo/bar')
+    False
+    """
     matchlen = len(matchwith)
     if (path.startswith(matchwith)
         and path[matchlen:matchlen+1] in [os.sep, '']):
@@ -15,6 +64,7 @@ def _dirmatch(path, matchwith):
 
 
 def _virtualenv_sys(venv_path):
+    "obtain version and path info from a virtualenv."
     executable = os.path.join(venv_path, 'bin', 'python')
     p = subprocess.Popen(['python',
         '-c', 'import sys;'
