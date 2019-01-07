e49954e4f049e0844896bdfda4401f07eba921c6
Ömer Fadıl USTA <omerusta@gmail.com>
2019-01-07 06:17:43 +0200

    Add python3.7 support and remove desupported 3.3 support.

--- setup.py
+++ setup.py
@@ -54,6 +54,7 @@ setup(name="virtualenv-clone",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
     tests_require=test_requirements,
     cmdclass={'test': ToxTest}
