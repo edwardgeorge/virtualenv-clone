75f0beda33c33289fb574e352282e8ff14ee415b
Nikita Obukhov <nickf.altlinux.org>
2020-03-12 12:48:29 +0300

    Fix build virtualenv-clone with python3.8  - Fix issue #59

--- setup.py
+++ setup.py
@@ -55,6 +55,7 @@ setup(name="virtualenv-clone",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     tests_require=test_requirements,
     cmdclass={'test': ToxTest}
