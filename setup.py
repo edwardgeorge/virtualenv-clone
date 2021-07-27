050b206f41d0ee83ebf211d1cbd0795287f733f9
Eeshan Garg <eeshan@zulip.com>
2021-07-27 16:23:04 -0230

    pypi: Prepare release for version 0.5.6.

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ class ToxTest(TestCommand):
 
 
 setup(name="virtualenv-clone",
-    version='0.5.5',
+    version='0.5.6',
     description='script to clone virtualenvs.',
     long_description=long_description,
     long_description_content_type='text/markdown',
